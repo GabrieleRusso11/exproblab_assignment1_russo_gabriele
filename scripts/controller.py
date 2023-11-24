8dbc1e986171fd3a8ab247da4f029eeb5d4d6ea4
GabrieleRusso11 <93495918+GabrieleRusso11@users.noreply.github.com>
2023-11-24 14:58:59 +0100

    Add files via upload

--- scripts/controller.py
+++ scripts/controller.py
@@ -2,38 +2,49 @@
 
 import random
 import rospy
+
 # Import constant name defined to structure the architecture.
 from exproblab_assignment1_russo_gabriele import architecture_names as anm
-# Import the ActionServer implementation used.
+
+# Import the simple ActionServer.
 from actionlib import SimpleActionServer
+
 # Import custom message, actions and services.
 from exproblab_assignment1_russo_gabriele.msg import ControlFeedback, ControlResult
 from exproblab_assignment1_russo_gabriele.srv import NewPosition
-import exproblab_assignment1_russo_gabriele  # This is required to pass the `PlanAction` type for instantiating the `SimpleActionServer`.
+import exproblab_assignment1_russo_gabriele  # This is required to pass the `ControlAction` type for 
+                                             # instantiating the `SimpleActionServer`.
+
+# used to color the output text
+import colorama
+from colorama import Fore
 
 # A tag for identifying logs producer.
 LOG_TAG = anm.NODE_CONTROLLER
 
 
 # An action server to simulate motion controlling.
-# Given a plan as a set of via points, it simulate the movements
-# to reach each point with a random delay. This server updates
+# Given a plan as a list of locations, it simulate the movements
+# to reach each location with a delay. This server updates
 # the current robot position stored in the `robot-state` node.
 class ControllingAction(object):
 
     def __init__(self):
-        # Get random-based parameters used by this server
-        self._random_motion_time = rospy.get_param(anm.PARAM_CONTROLLER_TIME, [0.1, 2.0])
+        # Get the delay ros parameter used by this server
+        # to simulate the movements
+        self.delay = rospy.get_param("/moving_time")
+
         # Instantiate and start the action server based on the `SimpleActionServer` class.
         self._as = SimpleActionServer(anm.ACTION_CONTROLLER,
                                       exproblab_assignment1_russo_gabriele.msg.ControlAction,
                                       execute_cb=self.execute_callback,
                                       auto_start=False)
         self._as.start()
+
         # Log information.
-        log_msg = (f'`{anm.ACTION_CONTROLLER}` Action Server initialised. It will navigate trough the plan with a delay ' 
-                   f'between each location spanning in [{self._random_motion_time[0]}, {self._random_motion_time[1]}).')
+        log_msg = (Fore.LIGHTCYAN_EX + f'`{anm.ACTION_CONTROLLER}` Action Server initialised.')
         rospy.loginfo(anm.tag_log(log_msg, LOG_TAG))
+        print(Fore.WHITE)
 
     # The callback invoked when a client set a goal to the `controller` server.
     # This function requires a list of via points (i.e., the plan), and it simulate
@@ -42,9 +53,11 @@ class ControllingAction(object):
     # As soon as each via point is reached, the related robot position is updated
     # in the `robot-state` node.
     def execute_callback(self, goal):
+        
         # Check if the provided plan is processable. If not, this service will be aborted.
         if goal is None or goal.plan is None or len(goal.plan) == 0:
-            rospy.logerr(anm.tag_log('No locations provided! This service will be aborted!', LOG_TAG))
+            rospy.logerr(anm.tag_log(Fore.LIGHTRED_EX + 'No locations provided! This service will be aborted!', LOG_TAG))
+            print(Fore.WHITE)
             self._as.set_aborted()
             return
 
@@ -54,33 +67,37 @@ class ControllingAction(object):
         for location in goal.plan:
             # Check that the client did not cancel this service.
             if self._as.is_preempt_requested():
-                rospy.loginfo(anm.tag_log('Service has been cancelled by the client!', LOG_TAG))
+                rospy.loginfo(anm.tag_log(Fore.LIGHTRED_EX + 'Service has been cancelled by the client!', LOG_TAG))
+                print(Fore.WHITE)
                 # Actually cancel this service.
                 self._as.set_preempted()
                 return
                 # Wait before to reach the following via point. This is just for testing purposes.
-            delay = random.uniform(self._random_motion_time[0], self._random_motion_time[1])
+            delay = self.delay
             rospy.sleep(delay)
             # Publish a feedback to the client to simulate that a via point has been reached. 
             feedback.reached_location = location
             self._as.publish_feedback(feedback)
             # Set the new current position into the `robot-state` node.
-            _set_pose_client(location)
+            _set_client_position(location)
             # Log current robot position.
-            log_msg = f'Reaching location ({location}).'
+            log_msg = Fore.LIGHTCYAN_EX + f'Reaching location ({location.name}).'
             rospy.loginfo(anm.tag_log(log_msg, LOG_TAG))
+            print(Fore.WHITE)
 
         # Publish the results to the client.
         result = ControlResult()
         result.final_location = feedback.reached_location
-        rospy.loginfo(anm.tag_log('Motion control successes.', LOG_TAG))
+        log_msg = Fore.CYAN + 'Motion control successes.'
+        rospy.loginfo(anm.tag_log(log_msg, LOG_TAG))
+        print(Fore.WHITE)
         self._as.set_succeeded(result)
         return  # Succeeded.
 
 
 # Update the current robot `pose` stored in the `robot-state` node.
 # This method is performed for each point provided in the action's server feedback.
-def _set_pose_client(position):
+def _set_client_position(position):
     # Eventually, wait for the server to be initialised.
     rospy.wait_for_service(anm.SERVER_SET_POSE)
     try:
@@ -91,7 +108,7 @@ def _set_pose_client(position):
         service = rospy.ServiceProxy(anm.SERVER_SET_POSE, NewPosition)
         service(position)  # The `response` is not used.
     except rospy.ServiceException as e:
-        log_msg = f'Server cannot set current robot position: {e}'
+        log_msg = Fore.LIGHTRED_EX + f'Server cannot set current robot position: {e}'
         rospy.logerr(anm.tag_log(log_msg, LOG_TAG))
 
 
--- scripts/state_machine.py
+++ scripts/state_machine.py
@@ -18,7 +18,7 @@ from smach import StateMachine, State
 from exproblab_assignment1_russo_gabriele import architecture_names as anm
 
 # Import used messages defined within the ROS architecture.
-from exproblab_assignment1_russo_gabriele.msg import Location, PlanAction, PlanGoal, ControlAction, ControlGoal
+from exproblab_assignment1_russo_gabriele.msg import Location, PlanAction, PlanGoal, ControlAction, ControlGoal, ControlResult
 from exproblab_assignment1_russo_gabriele.srv import NewPosition
 
 # Sleeping time (in seconds) of the waiting thread to allow the computations
@@ -49,6 +49,8 @@ STATE_RECHARGING = 'RECHARGING'             # The name of the state where the ro
 STATE_GO_INTO_A_ROOM = 'GO_INTO_A_ROOM'     # The name of the state where the robot moves toward a room
 STATE_WAIT_FOR_TOP_MAP = 'WAIT_FOR_TOP_MAP' # The name of the initial state where the robot waits in the room E for the topological map
 STATE_WAIT = 'WAIT'                         # The name of the state where the robot waits in the room after reached it
+STATE_PLANNING = 'PLANNING'
+STATE_MOVING = 'MOVING'
 
 # The list of names that identify the transitions of the Finite State Machine.
 TRANS_BATTERY_LOW = 'battery_low'           # The transition from the 'GO_INTO_A_ROOM' and 'WAIT' states toward the `RECHARGING` state.
@@ -57,7 +59,13 @@ TRANS_NO_TOP_MAP = 'no_top_map'             # The transition from the 'WAIT_FOR_
 TRANS_TOP_MAP_READY = 'top_map_ready'       # The transition from the 'WAIT_FOR_TOP_MAP' to 'GO_INTO_A_ROOM' 
 TRANS_IN_A_ROOM = 'in_a_room'               # The transition from the 'GO_INTO_A_ROOM' to 'WAIT'
 TRANS_TIMEOUT = 'timeout'                   # The transition from the 'WAIT' to 'GO_INTO_A_ROOM'
+TRANS_MOVING = 'moving'
+TRANS_SEARCH_CHARG_LOC = 'search_charging_location'
 
+VAR_PLAN = 'plan'
+VAR_CHARG_FEEDBACK = 'feedback' # boolean value that the planning state 
+                                # uses to comunicate to the Recharging state that 
+                                # the charging location is reached.
 
 # Sleeping time (in seconds) of the waiting thread to allow the computations
 # for getting stimulus from the other components of the architecture.
@@ -88,15 +96,19 @@ class WaitForTopMap(State):
                 else:
                     rospy.wait_for_service(anm.SERVER_SET_POSE)
                     self._interface.set_map()
-                    start_pos = self._interface._init_pos
+                    start_pos = Location(self._interface._init_pos)
                     service = rospy.ServiceProxy(anm.SERVER_SET_POSE, NewPosition)
                     service(start_pos)  # The `response` is not used.
 
                     if self._interface.end:
-                        print("the map is ready")
+                        print(Fore.WHITE)
+                        print(Fore.YELLOW + "the map is ready")
+                        print(Fore.WHITE)
                         return TRANS_TOP_MAP_READY
                     else:
-                        print("the map is not ready")
+                        print(Fore.WHITE)
+                        print(Fore.YELLOW + "the map is not ready")
+                        print(Fore.WHITE)
                         return TRANS_NO_TOP_MAP
             finally:
                 # Release the mutex to unblock the `self._helper` subscription threads if they are waiting.
@@ -108,7 +120,7 @@ class WaitForTopMap(State):
 
 # The definition of the go_into_a_room state.
 # It is the state where the robot goes toward a location (the urgent location that needs to be visited)
-class GoIntoARoom(State):
+class Planning(State):
 
     # Construct this class, i.e., initialise this state.
     def __init__(self,state_machine_interface):
@@ -117,46 +129,115 @@ class GoIntoARoom(State):
         self._interface = state_machine_interface
 
         # Initialise this state with possible transitions (i.e., valid outputs of the `execute` function).
-        State.__init__(self, outcomes=[TRANS_BATTERY_LOW, TRANS_IN_A_ROOM, TRANS_CHARGED_BATTERY, TRANS_TOP_MAP_READY, TRANS_TIMEOUT])
+        State.__init__(self, outcomes=[TRANS_MOVING, TRANS_CHARGED_BATTERY, TRANS_SEARCH_CHARG_LOC,
+                                       TRANS_TOP_MAP_READY, TRANS_TIMEOUT,TRANS_BATTERY_LOW], output_keys=[VAR_PLAN,VAR_CHARG_FEEDBACK])
 
     # Define the function performed each time a transition is such to enter in this state.
     def execute(self, userdata):
         # Define a random point to be reached through some via-points to be planned.
+        # disjoint all the individuals
+        self._interface.disjoint()
+
+        # activate the reasoner 
+        self._interface.reasoner()
+
+        actual_pos = self._interface.ask_rob_pos()
+    
+        print(Fore.LIGHTCYAN_EX + "robot position is :", Fore.WHITE)
+        print(str(actual_pos))
+        print()
+
         goal = PlanGoal()
-        goal.target = 
+
+        if self._interface.low_battery_mode:
+            goal.target = self._interface.search_charging_location()
+        else:
+            goal.target = self._interface.target_location()
+
         # Invoke the planner action server.
         self._interface.planner_client.send_goal(goal)
+
+        print(Fore.LIGHTBLUE_EX)
         rospy.loginfo(anm.tag_log('Planning to go in a new random position...', LOG_TAG))
+        print(Fore.WHITE)
 
 
         while not rospy.is_shutdown():  # Wait for stimulus from the other nodes of the architecture.
             # Acquire the mutex to assure data consistencies with the ROS subscription threads managed by `self._helper`.
             self._interface.mutex.acquire()
             try:
-                # If the battery is no low anymore take the `charged` transition.
-                if self._interface.is_battery_low():
+                if self._interface.is_battery_low() and not self._interface.low_battery_mode:  # Higher priority.
                     self._interface.planner_client.cancel_goals()
                     return TRANS_BATTERY_LOW
-                
                 if self._interface.planner_client.is_done():
-                    p = self._interface.planner_client.get_results().plan
-                    # Start the action server for moving the robot through the planned via-points.
-                    control_goal = ControlGoal(plan=p)
-                    self._interface.controller_client.send_goal(control_goal)
-                    rospy.loginfo(anm.tag_log('Following the plan to reach a random position...', LOG_TAG))
-
-                    if self._interface.is_battery_low():
-                        self._interface.controller_client.cancel_goals()
-                    return TRANS_BATTERY_LOW
-
-                if self._interface.controller_client.is_done():
-                    return TRANS_IN_A_ROOM
+                    userdata.plan = self._interface.planner_client.get_results().plan [1:]
+                    return TRANS_MOVING
             finally:
                 # Release the mutex to unblock the `self._helper` subscription threads if they are waiting.
                 self._interface.mutex.release()
             # Wait for a reasonably small amount of time to allow `self._helper` processing stimulus (eventually).
             rospy.sleep(LOOP_SLEEP_TIME)
 
+# The definition of the go_into_a_room state.
+# It is the state where the robot goes toward a location (the urgent location that needs to be visited)
+class Moving(State):
+
+    # Construct this class, i.e., initialise this state.
+    def __init__(self,state_machine_interface):
+
+        # Get a reference to the interfaces with the other nodes of the architecture.
+        self._interface = state_machine_interface
+
+        # Initialise this state with possible transitions (i.e., valid outputs of the `execute` function).
+        State.__init__(self, outcomes=[TRANS_BATTERY_LOW, TRANS_IN_A_ROOM, 
+                                       TRANS_MOVING], input_keys=[VAR_PLAN])
+
+    # Define the function performed each time a transition is such to enter in this state.
+    def execute(self, userdata):
+
+        old_location = self._interface.ask_rob_pos()
+        print()
+        print("old location: ")
+        print(old_location)
+
+        # Start the action server for moving the robot through the planned via-points.
+        control_goal = ControlGoal()
+        control_goal.plan = userdata.plan
+        self._interface.controller_client.send_goal(control_goal)
+
+        print(Fore.LIGHTRED_EX)
+        rospy.loginfo(anm.tag_log('Following the plan to reach the target location...', LOG_TAG))
+        print(Fore.WHITE)
+
+        while not rospy.is_shutdown():  # Wait for stimulus from the other nodes of the architecture.
+            # Acquire the mutex to assure data consistencies with the ROS subscription threads managed by `self._helper`.
+            self._interface.mutex.acquire()
+            try:
+
+                if self._interface.is_battery_low() and not self._interface.low_battery_mode:  # Higher priority.
+                    self._interface.controller_client.cancel_goals()
+                    return TRANS_BATTERY_LOW
+
+                if self._interface.controller_client.is_done():
+                    result = self._interface.controller_client.get_results()
+                    new_location = result.final_location.name
+                    print("new position")
+                    print(new_location)
+                    self._interface.update_robot_position(str(old_location),str(new_location))
+                    self._interface.update_robot_time()
+                    self._interface.update_location_time(str(new_location))
+                    # activate the reasoner 
+                    self._interface.reasoner()
+                    if self._interface.is_battery_low():
+                        return TRANS_BATTERY_LOW
+                    else:
+                        return TRANS_IN_A_ROOM
+                    
+            finally:
+                # Release the mutex to unblock the `self._helper` subscription threads if they are waiting.
+                self._interface.mutex.release()
+            # Wait for a reasonably small amount of time to allow `self._helper` processing stimulus (eventually).
+            rospy.sleep(LOOP_SLEEP_TIME)
 
 # The definition of the recharging state.
 # It is the state where the robot recharges its battery
@@ -169,19 +250,50 @@ class Recharging(State):
         self._interface = state_machine_interface
 
         # Initialise this state with possible transitions (i.e., valid outputs of the `execute` function).
-        State.__init__(self, outcomes=[TRANS_BATTERY_LOW, TRANS_CHARGED_BATTERY])
+        State.__init__(self, outcomes=[TRANS_BATTERY_LOW, TRANS_CHARGED_BATTERY, 
+                                       TRANS_SEARCH_CHARG_LOC], input_keys=[VAR_CHARG_FEEDBACK])
+
+    
 
     # Define the function performed each time a transition is such to enter in this state.
     # Note that the input parameter `userdata` is not used since no data is required from the other states.
     def execute(self, userdata):
-         while not rospy.is_shutdown():  # Wait for stimulus from the other nodes of the architecture.
+        
+        # check if the robot is already in the charging location
+        robot_is = self._interface.ask_rob_pos()
+        if robot_is == self._interface.charging_loc:
+            self._interface.found = True
+            self._interface.set_starting_flag(self._interface.found)
+            print(Fore.WHITE)
+            print(Fore.LIGHTGREEN_EX + "The Robot is in the Charging Location.")
+            print()
+            print(Fore.LIGHTGREEN_EX + "...Wait for charging...")
+            print(Fore.WHITE)            
+        else:
+            print(Fore.WHITE)
+            print(Fore.LIGHTCYAN_EX + "Searching the charging location mode...")
+            print(Fore.WHITE)
+            self._interface.low_battery_mode = True
+
+         
+        
+
+        while not rospy.is_shutdown():  # Wait for stimulus from the other nodes of the architecture.
             # Acquire the mutex to assure data consistencies with the ROS subscription threads managed by `self._helper`.
             self._interface.mutex.acquire()
             try:
                 # If the battery is no low anymore take the `charged` transition.
                 if not self._interface.is_battery_low():
+                    print()
+                    print((Fore.LIGHTCYAN_EX + "The battery is charged."))
+                    print()
                     self._interface.reset_states()  # Reset the state variable related to the stimulus.
+                    self._interface.set_starting_flag(self._interface.found)
                     return TRANS_CHARGED_BATTERY
+                if self._interface.is_battery_low() and not self._interface.found:
+                    return TRANS_SEARCH_CHARG_LOC
+                #if self._interface.is_battery_low() and feedback:
+                #    return TRANS_BATTERY_LOW
                 # Note that if unexpected stimulus comes from the other nodes of the architecture through the
                 # `self._helper` class, then this state will not take any transitions. This is equivalent to have a
                 # loop-like transition in the behavioural UML diagram for all the other stimulus except `TRAMS_RECHARGED`.
@@ -207,7 +319,7 @@ class Wait(State):
 
     # Define the function performed each time a transition is such to enter in this state.
     def execute(self, userdata):
-        waiting_time = rospy.get_param("/time")
+        waiting_time = rospy.get_param("/waiting_time")
         while not rospy.is_shutdown():  # Wait for stimulus from the other nodes of the architecture.
             # Acquire the mutex to assure data consistencies with the ROS subscription threads managed by `self._helper`.
             self._interface.mutex.acquire()
@@ -216,6 +328,9 @@ class Wait(State):
                 if self._interface.is_battery_low():
                     return TRANS_BATTERY_LOW
                 else:
+                    print(Fore.WHITE)
+                    print(Fore.CYAN + "...WAITING...")
+                    print(Fore.WHITE)
                     rospy.sleep(waiting_time)
                     return TRANS_TIMEOUT
                 # Note that if unexpected stimulus comes from the other nodes of the architecture through the
@@ -245,27 +360,34 @@ def main():
         # Define the inner state to plan the via-points toward a random position.
         StateMachine.add(STATE_WAIT_FOR_TOP_MAP, WaitForTopMap(interface),
                             transitions={TRANS_NO_TOP_MAP: STATE_WAIT_FOR_TOP_MAP,
-                                        TRANS_TOP_MAP_READY: STATE_GO_INTO_A_ROOM})
+                                        TRANS_TOP_MAP_READY: STATE_PLANNING})
 
         # Define the inner state to move to a random position.
-        StateMachine.add(STATE_GO_INTO_A_ROOM, GoIntoARoom(interface),
-                            transitions={TRANS_IN_A_ROOM: STATE_WAIT,
+        StateMachine.add(STATE_PLANNING, Planning(interface),
+                            transitions={TRANS_TOP_MAP_READY: STATE_PLANNING,
+                                        TRANS_MOVING: STATE_MOVING,
+                                        TRANS_CHARGED_BATTERY: STATE_PLANNING,
                                         TRANS_BATTERY_LOW: STATE_RECHARGING,
-                                        TRANS_TOP_MAP_READY: STATE_GO_INTO_A_ROOM,
-                                        TRANS_TIMEOUT: STATE_GO_INTO_A_ROOM,
-                                        TRANS_CHARGED_BATTERY: STATE_GO_INTO_A_ROOM})
-
+                                        TRANS_SEARCH_CHARG_LOC: STATE_PLANNING,
+                                        TRANS_TIMEOUT: STATE_PLANNING})
         
+        # Define the inner state to move to a random position.
+        StateMachine.add(STATE_MOVING, Moving(interface),
+                            transitions={TRANS_BATTERY_LOW: STATE_RECHARGING,
+                                        TRANS_MOVING: STATE_MOVING,
+                                        TRANS_IN_A_ROOM: STATE_WAIT})
+
         # Define the inner state to plan the via-points toward the user.
         StateMachine.add(STATE_WAIT, Wait(interface),
-                            transitions={TRANS_TIMEOUT: STATE_GO_INTO_A_ROOM,
+                            transitions={TRANS_TIMEOUT: STATE_PLANNING,
                                         TRANS_BATTERY_LOW: STATE_RECHARGING,
                                         TRANS_IN_A_ROOM: STATE_WAIT})
 
         # Define the inner state to move toward the user.
         StateMachine.add(STATE_RECHARGING, Recharging(interface),
-                            transitions={TRANS_CHARGED_BATTERY: STATE_GO_INTO_A_ROOM,
-                                         TRANS_BATTERY_LOW: STATE_RECHARGING})
+                            transitions={TRANS_CHARGED_BATTERY: STATE_PLANNING,
+                                         TRANS_BATTERY_LOW: STATE_RECHARGING,
+                                         TRANS_SEARCH_CHARG_LOC: STATE_PLANNING})
 
     # Create and start the introspection server for visualizing the finite state machine.
     intro_server = smach_ros.IntrospectionServer('intro_server', sm, '/SM_ROOT')
